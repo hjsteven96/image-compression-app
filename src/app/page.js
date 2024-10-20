6b1ac3b785c913ddac18a7fb4107bab3ed49518e
emory <hjsteve96@gmail.com>
2024-10-20 13:52:13 +0900

    v1.7

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,25 @@
 import ImageCompressor from "./components/ImageCompressor";
+import Head from "next/head";
 
 export default function Home() {
     return (
-        <main className="min-h-screen bg-gray-100 py-12">
-            <ImageCompressor />
-        </main>
+        <>
+            <Head>
+                <title>심플 이미지 압축</title>
+                <meta
+                    name="description"
+                    content="가장 빠른 간편한 이미지 압축"
+                />
+                <meta property="og:title" content="이미지 압축" />
+                <meta
+                    property="og:description"
+                    content="가장 빠른 간편한 이미지 압축"
+                />
+                <meta property="og:type" content="website" />
+            </Head>
+            <main className="min-h-screen bg-gray-100 py-12">
+                <ImageCompressor />
+            </main>
+        </>
     );
 }
